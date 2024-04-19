bbf5328c24a101032cc20a2b6b7059cf506c1a09
Rizzo Domenico <domy1riz@gmail.com>
2024-04-19 10:59:45 +0200

    Window minimum size added and Url entry placeholder logic implemented

--- mainGUI.py
+++ mainGUI.py
@@ -3,6 +3,18 @@ from tkinter import filedialog
 from downloader import download_media
 
 
+def on_entry_click(url_entry):
+    if url_entry.get() == "Enter the YouTube video URL":
+        url_entry.delete(0, "end")
+        url_entry.config(fg='black')
+
+
+def on_focusout(url_entry):
+    if not url_entry.get():
+        url_entry.insert(0, "Enter the YouTube video URL")
+        url_entry.config(fg='grey')
+
+
 def browse_button_clicked(url_entry, format_choice, status_label):
     url = url_entry.get()
     save_directory = filedialog.askdirectory()
@@ -13,10 +25,17 @@ def browse_button_clicked(url_entry, format_choice, status_label):
 
 
 def build_components():
-    url_label = tk.Label(root, text="Enter the YouTube video URL:")
-    url_label.pack()
+    welcome_label = tk.Label(root, text="Welcome to YouTube Media Downloader", font=("Helvetica", 18))
+    welcome_label.pack(pady=20)
 
-    url_entry = tk.Entry(root, width=50)
+    description_label = tk.Label(root, text="To begin, please paste a YouTube URL, select the desired format type, "
+                                            "and specify the save directory", font=14)
+    description_label.pack(pady=(0, 20))
+
+    url_entry = tk.Entry(root, width=50, fg='grey')
+    url_entry.insert(0, "Enter the YouTube video URL")
+    url_entry.bind("<FocusIn>", lambda event: on_entry_click(url_entry))
+    url_entry.bind("<FocusOut>", lambda event: on_focusout(url_entry))
     url_entry.pack()
 
     format_choice = tk.StringVar()
@@ -39,7 +58,7 @@ def build_components():
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("YouTube Media Downloader")
-    root.geometry("600x300")
+    root.minsize(800, 400)
 
     build_components()
 
