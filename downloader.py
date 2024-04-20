d3903abb3acc8454785210b48577823ee9d25db2
Rizzo Domenico <domy1riz@gmail.com>
2024-04-20 14:20:48 +0200

    fixed a bug for mp3 choice

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 import moviepy.editor as mp
+import os
 
 
 def download_media(url, save_path, format_choice, status_label):
@@ -13,15 +14,16 @@ def download_media(url, save_path, format_choice, status_label):
             else:
                 status_label.config(text="No progressive stream available for the video.")
         elif format_choice == "mp3":
-            audio = yt.streams.filter(only_audio=True).first()
-            if audio:
-                audio_file_path = f"{save_path}/{yt.title}.mp3"
-                audio.download(output_path=save_path)
-                clip = mp.AudioFileClip(audio_file_path)
-                clip.write_audiofile(audio_file_path)
-                clip.close()
-                status_label.config(text="Audio downloaded successfully")
-            else:
-                status_label.config(text="No audio stream available for the video.")
+            video_file_path = f"{save_path}/{yt.title}.mp4"
+            video_stream = yt.streams.filter(file_extension="mp4").first()
+            video_stream.download(output_path=save_path)
+            video_clip = mp.VideoFileClip(video_file_path)
+            audio_clip = video_clip.audio
+            audio_file_path = f"{save_path}/{yt.title}.mp3"
+            audio_clip.write_audiofile(audio_file_path)
+            video_clip.close()
+            audio_clip.close()
+            os.remove(video_file_path)
+            status_label.config(text="Audio downloaded successfully")
     except Exception as e:
         status_label.config(text=f"Error: {e}")
