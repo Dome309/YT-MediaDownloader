3263e6582c5afeb0015982071d8a407e7a4d19c8
Rizzo Domenico <domy1riz@gmail.com>
2024-04-24 21:40:18 +0200

    gui icon has been set

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from gui import build_gui, center_window
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("YouTube Media Downloader")
+    icon_image = tk.PhotoImage(file="images/logo.png")
+    root.iconphoto(True, icon_image)
+
     window_width = 900
     window_height = 500
     root.minsize(800, 400)
